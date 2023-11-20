98872221fb4656ce15b34c2ae1f0b2c16248ed15
Mirosław Sarna <miroslaw.sarna@gmail.com>
2023-11-20 01:33:36 +0100

    Added model for settings

---
+++ src/main/java/com/mso/ph/programhelper/settings/model/DataSettings.java
@@ -0,0 +1,13 @@
+package com.mso.ph.programhelper.settings.model;
+
+public class DataSettings {
+    private String dataFolderPath;
+
+    public String getDataFolderPath() {
+        return dataFolderPath;
+    }
+
+    public void setDataFolderPath(String dataFolderPath) {
+        this.dataFolderPath = dataFolderPath;
+    }
+}
---
+++ src/main/java/com/mso/ph/programhelper/settings/model/WindowState.java
@@ -0,0 +1,40 @@
+package com.mso.ph.programhelper.settings.model;
+
+public class WindowState {
+    public double x;
+    public double y;
+    public double width;
+    public double height;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
